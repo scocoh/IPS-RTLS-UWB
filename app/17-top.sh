101a4152d42f2f89066af6aa515505c394c534c7
Scott Cohen <scott@asproj.com>
2025-03-31 17:10:16 +0000

    Saved changes before stopping on 20250331_171016

---
+++ app/17-top.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# DESC: Launches top to monitor system processes
+# VERSION 0P.1B.01
+
+echo "Launching top..."
+top
---
+++ app/18-ps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# DESC: Runs ps to list running processes
+# VERSION 0P.1B.01
+
+echo "Listing processes with ps..."
+ps aux
+echo "Press Enter to return to the menu..."
+read
